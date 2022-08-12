5bb4f68830e00408c7fb37964e38a49de5c641ea
marcelo <marceloferreiradossantos@hotmail.com>
2022-08-11 21:04:29 -0300

    [bugsfixes] randonmize songs selection+bugfixes

--- controllers/spotify.js
+++ controllers/spotify.js
@@ -63,23 +63,28 @@ exports.getSongs = async (req, res, next) => {
       headers
     );
     const genre = req.query.genre
-    console.log(req)
-    const songsResponse = await axios.get(`https://api.spotify.com/v1/search?q=genre:${genre}&type=track&limit=4`, {
+    const qtd = req.query.qtd > 10 ? 10 : req.query.qtd
+    const randomOffset = Math.floor(Math.random() * 950)
+    const songsResponse = await axios.get(`https://api.spotify.com/v1/search?q=genre:${genre}&type=track&limit=50&offset=${randomOffset}`, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + response.data.access_token
       }
     })
     const songs = songsResponse.data
+
+    const songsPreview = songs.tracks.items.filter((e) => e.preview_url !== null)
+
     const parsedResponse = []
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < qtd; i++) {
       parsedResponse.push({
-        artist: songs.tracks.items[i].album.artists[0].name,
-        image: songs.tracks.items[i].album.images[0].url,
-        song: songs.tracks.items[i].name,
-        audio: songs.tracks.items[i].preview_url
+        artist: songsPreview[i].album.artists[0].name,
+        image: songsPreview[i].album.images[0].url,
+        song: songsPreview[i].name,
+        audio: songsPreview[i].preview_url
       })
     }
+
     res.send(parsedResponse)
   } catch (error) {
     console.log(error);
